e3bd77aa431f3b8e427cd1685756719525bc53d2
yc zhang <66052121+itt-icc@users.noreply.github.com>
2021-08-14 01:11:07 +0800

    Update 模板.cpp

--- 模板.cpp
+++ 模板.cpp
@@ -1,60 +1,60 @@
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//
-///* ÇëÔÚÕâÀïÌîĞ´´ğ°¸ */
-//
-//template <class T>
-//void sort(T* a, int size)
-//{
-//    for (int i = 0; i < size; i++)
-//        cin >> a[i];
-//
-//    /*Ñ¡ÔñÅÅĞò*/
-//    for (int i = 0; i < size-1; ++i)
-//    {
-//        int index = i;
-//        /*ÕÒµ½×îĞ¡Öµ¶ÔÓ¦ÏÂ±ê*/
-//        for (int j = i + 1; j < size; j++)
-//        {
-//            if (a[index] > a[j])
-//            {
-//                index = j;
-//            }
-//        }
-//        T temp = a[index];
-//        a[index] = a[i];
-//        a[i] = temp;
-//    }
-//}
-//
-//
-//
-//template <class T>
-//void display(T* a, int size) {
-//    for (int i = 0; i < size - 1; i++) cout << a[i] << ' ';
-//    cout << a[size - 1] << endl;
-//}
-//int main() {
-//    const int SIZE = 10;
-//    int a[SIZE];
-//    char b[SIZE];
-//    double c[SIZE];
-//    string d[SIZE];
-//    int ty, size;
-//    cin >> ty;
-//    while (ty > 0) {
-//        cin >> size;
-//        switch (ty) {
-//        case 1:sort(a, size); display(a, size); break;
-//        case 2:sort(b, size); display(b, size); break;
-//        case 3:sort(c, size); display(c, size); break;
-//        case 4:sort(d, size); display(d, size); break;
-//        }
-//        cin >> ty;
-//    }
-//    return 0;
-//}
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* è¯·åœ¨è¿™é‡Œå¡«å†™ç­”æ¡ˆ */
+
+template <class T>
+void sort(T* a, int size)
+{
+   for (int i = 0; i < size; i++)
+       cin >> a[i];
+
+   /*é€‰æ‹©æ’åº*/
+   for (int i = 0; i < size-1; ++i)
+   {
+       int index = i;
+       /*æ‰¾åˆ°æœ€å°å€¼å¯¹åº”ä¸‹æ ‡*/
+       for (int j = i + 1; j < size; j++)
+       {
+           if (a[index] > a[j])
+           {
+               index = j;
+           }
+       }
+       T temp = a[index];
+       a[index] = a[i];
+       a[i] = temp;
+   }
+}
+
+
+
+template <class T>
+void display(T* a, int size) {
+   for (int i = 0; i < size - 1; i++) cout << a[i] << ' ';
+   cout << a[size - 1] << endl;
+}
+int main() {
+   const int SIZE = 10;
+   int a[SIZE];
+   char b[SIZE];
+   double c[SIZE];
+   string d[SIZE];
+   int ty, size;
+   cin >> ty;
+   while (ty > 0) {
+       cin >> size;
+       switch (ty) {
+       case 1:sort(a, size); display(a, size); break;
+       case 2:sort(b, size); display(b, size); break;
+       case 3:sort(c, size); display(c, size); break;
+       case 4:sort(d, size); display(d, size); break;
+       }
+       cin >> ty;
+   }
+   return 0;
+}
 
 
 
@@ -63,85 +63,85 @@ Templatize the fibonacci( ) function on the type of value that it produces
 (so it can produce long, float, etc. instead of just int).
 **/
 
-//
-//#include <iostream>
-//using namespace std;
-//template <class T>
-//T fibonacci(T n) {
-//    const int size = 50;
-//    static T f[size];
-//    f[0] = f[1] = 1;
-//
-//    int i;
-//    for (i = 0; i < size; i++)
-//        if (f[i] == 0) break;
-//    while (i <= n) {
-//        f[i] = f[i - 1] + f[i - 2];
-//        i++;
-//    }
-//    return f[n];
-//}
-//
-//
-//int main()
-//{
-//    cout << fibonacci(30) << endl;
-//    return 0;
-//}
 
-//#include<iostream>
-//using namespace std;
-////ÀàÄ£°å
-//template<class T>
-//class vector
-//{
-//public:
-//	void sort(T a[], int n)
-//	{
-//		T temp;
-//		bool exchange;
-//		for (int i = 1; i < n; i++)
-//		{
-//			exchange = false;
-//			for (int j = n - 1; j >= i; j--)
-//				if (a[j] < a[j - 1])
-//				{
-//					temp = a[j]; a[j] = a[j - 1]; a[j - 1] = temp;
-//					exchange = true;
-//				}
-//			if (!exchange)
-//				return;
-//		}
-//	}
-//	T sum(T a[], int n)
-//	{
-//		T sum = a[0];
-//		for (int i = 1; i < n; i++)
-//			sum += a[i];
-//		return sum;
-//	}
-//	int search(T e, T a[], int n)
-//	{
-//		for (int i = 0; i < n; i++)
-//			if (a[i] == e)
-//				return i;
-//		return -1;
-//	}
-//};
-//
-//void main()
-//{
-//	int data[5] = { 5,3,1,2,4 };
-//	vector<int> obj;
-//	cout << "Êı×éºÍÎª£º" << obj.sum(data, 5) << endl;
-//	cout << "Êı×Ö4ÔÚÊı×éÖĞµÄÎ»ÖÃÊÇ£º" << obj.search(4, data, 5) << endl;
-//	cout << "ÅÅĞòÇ°Êı×é¸÷ÔªËØÎª£º" << endl;
-//	for (int i = 0; i < 5; i++)
-//		cout << data[i] << " ";
-//	cout << endl;
-//	obj.sort(data,5);
-//	cout << "ÅÅĞòºóÊı×é¸÷ÔªËØÎª£º" << endl;
-//	for (int i = 0; i < 5; i++)
-//		cout << data[i] << " ";
-//	cout << endl;
-//}
+#include <iostream>
+using namespace std;
+template <class T>
+T fibonacci(T n) {
+   const int size = 50;
+   static T f[size];
+   f[0] = f[1] = 1;
+
+   int i;
+   for (i = 0; i < size; i++)
+       if (f[i] == 0) break;
+   while (i <= n) {
+       f[i] = f[i - 1] + f[i - 2];
+       i++;
+   }
+   return f[n];
+}
+
+
+int main()
+{
+   cout << fibonacci(30) << endl;
+   return 0;
+}
+
+#include<iostream>
+using namespace std;
+//ç±»æ¨¡æ¿
+template<class T>
+class vector
+{
+public:
+	void sort(T a[], int n)
+	{
+		T temp;
+		bool exchange;
+		for (int i = 1; i < n; i++)
+		{
+			exchange = false;
+			for (int j = n - 1; j >= i; j--)
+				if (a[j] < a[j - 1])
+				{
+					temp = a[j]; a[j] = a[j - 1]; a[j - 1] = temp;
+					exchange = true;
+				}
+			if (!exchange)
+				return;
+		}
+	}
+	T sum(T a[], int n)
+	{
+		T sum = a[0];
+		for (int i = 1; i < n; i++)
+			sum += a[i];
+		return sum;
+	}
+	int search(T e, T a[], int n)
+	{
+		for (int i = 0; i < n; i++)
+			if (a[i] == e)
+				return i;
+		return -1;
+	}
+};
+
+void main()
+{
+	int data[5] = { 5,3,1,2,4 };
+	vector<int> obj;
+	cout << "æ•°ç»„å’Œä¸ºï¼š" << obj.sum(data, 5) << endl;
+	cout << "æ•°å­—4åœ¨æ•°ç»„ä¸­çš„ä½ç½®æ˜¯ï¼š" << obj.search(4, data, 5) << endl;
+	cout << "æ’åºå‰æ•°ç»„å„å…ƒç´ ä¸ºï¼š" << endl;
+	for (int i = 0; i < 5; i++)
+		cout << data[i] << " ";
+	cout << endl;
+	obj.sort(data,5);
+	cout << "æ’åºåæ•°ç»„å„å…ƒç´ ä¸ºï¼š" << endl;
+	for (int i = 0; i < 5; i++)
+		cout << data[i] << " ";
+	cout << endl;
+}
